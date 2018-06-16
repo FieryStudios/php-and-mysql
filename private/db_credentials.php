bfada709d1cd542a9505889da970108ff91bc5b2
Jane Irwin <fierystudios@iMac.local>
2018-06-16 16:50:32 -0400

    deleting credential file from repo

--- private/db_credentials.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DB_SERVER', 'kplresume.db.12860184.e11.hostedresource.net');
-define('DB_USER', 'kplresume');
-define('DB_PASS', 'ANds1!gIqb5Z');
-define('DB_NAME', 'kplresume');
-define('TABLE_NAME', 'collection');
-
-?>
